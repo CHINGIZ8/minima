a74f5acb91b2a86b9ead4506b13b4ff278be0295
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2022-12-28 14:30:30 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -44,15 +44,7 @@ main() {
 		local t_nv="\nВерсия ноды:            ${C_LGn}%s${RES}"
 		local t_lb="Последний блок:         ${C_LGn}%s${RES}\n"
 		
-		local t_ni="\nID ноды:                ${C_LGn}%s${RES}"
-		local t_rc="Реферальный код:        ${C_LGn}%s${RES}"
-		local t_lp="Последний сигнал:       ${C_LGn}%s${RES} (UTC)"
 		
-		local t_r="\n\n\tНаграды\n"
-		local t_af="Нода после форка:       ${C_LGn}%d${RES}"
-		local t_bf="Нода до форка:          ${C_LGn}%d${RES}"
-		local t_cc="Вклад в сообщество:     ${C_LGn}%d${RES}"
-		local t_ir="Приглашение рефералов:  ${C_LGn}%d${RES}"
 		
 	# Send Pull request with new texts to add a language - https://github.com/CHINGIZ8/minima/main/node_info.sh
 	#elif [ "$language" = ".." ]; then
@@ -68,16 +60,7 @@ main() {
 		local t_re="\n${C_R}Either the node is not registered, or the RPC does not work correctly, which cannot be fixed!${RES}\n"
 		local t_nv="\nNode version:            ${C_LGn}%s${RES}"
 		local t_lb="Latest block height:     ${C_LGn}%s${RES}\n"
-		
-		local t_ni="Node ID:                 ${C_LGn}%s${RES}"
-		local t_rc="Refferal code:           ${C_LGn}%s${RES}"
-		local t_lp="Last ping:               ${C_LGn}%s${RES} (UTC)"
-		
-		local t_r="\n\n\tRewards\n"
-		local t_af="Node after fork:         ${C_LGn}%d${RES}"
-		local t_bf="Node before fork:        ${C_LGn}%d${RES}"
-		local t_cc="Community contribution:  ${C_LGn}%d${RES}"
-		local t_ir="Inviting referrals:      ${C_LGn}%d${RES}"
+
 	fi
 
 	# Actions
@@ -90,16 +73,8 @@ main() {
 	
 	local node_id=`jq -r ".response.uid" <<< "$incentivecash"`
 	if [ -n "$node_id" ]; then
-		local node_id_hidden=`printf "$node_id" | sed 's%.*-.*-.*- *%...-%'`
-		local referral_code=`jq -r ".response.details.inviteCode" <<< "$incentivecash"`
-		local last_ping=`jq -r ".response.details.lastPing" <<< "$incentivecash"`
-		local last_ping_unix=`date --date "$last_ping" +"%s"`
-		local last_ping_human=`date --date "$last_ping" +"%d.%m.%y %H:%M" -u`
 		
-		local after_fork=`jq -r ".response.details.rewards.dailyRewards" <<< "$incentivecash"`
-		local before_fork=`jq -r ".response.details.rewards.previousRewards" <<< "$incentivecash"`
-		local community_contribution=`jq -r ".response.details.rewards.communityRewards" <<< "$incentivecash"`
-		local inviting_referrals=`jq -r ".response.details.rewards.inviterRewards" <<< "$incentivecash"`
+		
 	fi
 	
 	# Output
