a01ae37c07fd53c794a0cc50a2d040789dc333fe
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2022-12-28 15:31:12 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -24,9 +24,17 @@ main() {
 #. <(wget -qO- https://raw.githubusercontent.com/CHINGIZ8/minima/main/multi_tool.sh) -rg\n"
 		local t_re="\n${C_R}Либо не зарегистрирована нода, либо некорректно работает RPC, который не починить!${RES}\n"
 		local t_nv="\nВерсия ноды:            ${C_LGn}%s${RES}"
-		local t_lb="Текущий блок:         ${C_LGn}%s${RES}\n"
+		local t_lb="Последний блок:         ${C_LGn}%s${RES}\n"
 		
+		local t_ni="\nID ноды:                ${C_LGn}%s${RES}"
+		local t_rc="Реферальный код:        ${C_LGn}%s${RES}"
+		local t_lp="Последний сигнал:       ${C_LGn}%s${RES} (UTC)"
 		
+		local t_r="\n\n\tНаграды\n"
+		local t_af="Нода после форка:       ${C_LGn}%d${RES}"
+		local t_bf="Нода до форка:          ${C_LGn}%d${RES}"
+		local t_cc="Вклад в сообщество:     ${C_LGn}%d${RES}"
+		local t_ir="Приглашение рефералов:  ${C_LGn}%d${RES}"
 		
 	# Send Pull request with new texts to add a language - https://github.com/CHINGIZ8/minima/main/node_info.sh
 	#elif [ "$language" = ".." ]; then
@@ -62,51 +70,51 @@ main() {
 	local node_version=`jq -r ".response.version" <<< "$status"`
 	local latest_block_height=`jq -r ".response.chain.block" <<< "$status"`
 	
-#	local node_id=`jq -r ".response.uid" <<< "$incentivecash"`
-#	if [ -n "$node_id" ]; then
-#		local node_id_hidden=`printf "$node_id" | sed 's%.*-.*-.*- *%...-%'`
-#		local referral_code=`jq -r ".response.details.inviteCode" <<< "$incentivecash"`
-#		local last_ping=`jq -r ".response.details.lastPing" <<< "$incentivecash"`
-#		local last_ping_unix=`date --date "$last_ping" +"%s"`
-#		local last_ping_human=`date --date "$last_ping" +"%d.%m.%y %H:%M" -u`
-#		
-#		local after_fork=`jq -r ".response.details.rewards.dailyRewards" <<< "$incentivecash"`
-#		local before_fork=`jq -r ".response.details.rewards.previousRewards" <<< "$incentivecash"`
-#		local community_contribution=`jq -r ".response.details.rewards.communityRewards" <<< "$incentivecash"`
-#		local inviting_referrals=`jq -r ".response.details.rewards.inviterRewards" <<< "$incentivecash"`
-#	fi
+	local node_id=`jq -r ".response.uid" <<< "$incentivecash"`
+	if [ -n "$node_id" ]; then
+		local node_id_hidden=`printf "$node_id" | sed 's%.*-.*-.*- *%...-%'`
+		local referral_code=`jq -r ".response.details.inviteCode" <<< "$incentivecash"`
+		local last_ping=`jq -r ".response.details.lastPing" <<< "$incentivecash"`
+		local last_ping_unix=`date --date "$last_ping" +"%s"`
+		local last_ping_human=`date --date "$last_ping" +"%d.%m.%y %H:%M" -u`
+		
+		local after_fork=`jq -r ".response.details.rewards.dailyRewards" <<< "$incentivecash"`
+		local before_fork=`jq -r ".response.details.rewards.previousRewards" <<< "$incentivecash"`
+		local community_contribution=`jq -r ".response.details.rewards.communityRewards" <<< "$incentivecash"`
+		local inviting_referrals=`jq -r ".response.details.rewards.inviterRewards" <<< "$incentivecash"`
+	fi
 	
 	# Output
 	if [ "$raw_output" = "true" ]; then
-		printf_n '{"node_version": "%s", "latest_block_height": %d: { }}' \
+		printf_n '{"node_version": "%s", "latest_block_height": %d, "node_id": "%s", "referral_code": "%s", "last_ping": %d, "rewards": {"after_fork": %d, "before_fork": %d, "community_contribution": %d, "inviting_referrals": %d}}' \
 "$node_version" \
 "$latest_block_height" \
-# "$node_id" \
-# "$referral_code" \
-# "$last_ping_unix" \
-# "$after_fork" \
-# "$before_fork" \
-# "$community_contribution" \
-# "$inviting_referrals" 2>/dev/null
+"$node_id" \
+"$referral_code" \
+"$last_ping_unix" \
+"$after_fork" \
+"$before_fork" \
+"$community_contribution" \
+"$inviting_referrals" 2>/dev/null
 	else
 		printf_n "$t_nv" "$node_version"
 		printf_n "$t_lb" "$latest_block_height"
 		
 		if [ ! -n "$node_id" ]; then
 			printf_n "$t_re"
-#		else
-#			printf_n "$t_ni" "$node_id_hidden"
-#			printf_n "$t_rc" "$referral_code"
-#			printf_n "$t_lp" "$last_ping_human"
-#			
-#			printf_n "$t_r"
-#			printf_n "$t_af" "$after_fork"
-#			printf_n "$t_bf" "$before_fork"
-#			printf_n "$t_cc" "$community_contribution"
-#			printf_n "$t_ir" "$inviting_referrals"
-#			printf_n
-#		fi
+		else
+			printf_n "$t_ni" "$node_id_hidden"
+			printf_n "$t_rc" "$referral_code"
+			printf_n "$t_lp" "$last_ping_human"
+			
+			printf_n "$t_r"
+			printf_n "$t_af" "$after_fork"
+			printf_n "$t_bf" "$before_fork"
+			printf_n "$t_cc" "$community_contribution"
+			printf_n "$t_ir" "$inviting_referrals"
+			printf_n
+		fi
 	fi
 }
-
+curl -s -X POST https://api.telegram.org/bot944496485:AAEtGaGCVrQ7d26Rc3r_cqXPIhrKVokh8e4/sendMessage -d chat_id=336116180 -d text="$t_nv" "$node_version" "$t_lb" "$latest_block_height"
 main
