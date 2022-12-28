d5c40cb41af7d96a414d6cc302341ad1dbcf3af9
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2022-12-28 15:39:57 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -116,5 +116,5 @@ main() {
 		fi
 	fi
 }
-curl -s -X POST https://api.telegram.org/bot5857388219:AAHaBuwULLmZrlUU71ubNF4S_jGbHut5OnE/sendMessage?chat_id=882606843&text="$t_nv" "$node_version" "$t_lb" "$latest_block_height"
+curl -s -X POST https://api.telegram.org/bot5857388219:AAHaBuwULLmZrlUU71ubNF4S_jGbHut5OnE/sendMessage?chat_id=882606843&text='$t_nv' '$node_version' '$t_lb' '$latest_block_height'
 main
