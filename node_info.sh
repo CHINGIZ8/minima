c30eaa157e313035c380c2b653f0d9ff637c644e
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2022-12-28 14:45:32 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -7,31 +7,6 @@ raw_output="false"
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/CHINGIZ8/minima/main/colors.sh) --
 option_value(){ echo $1 | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
-while test $# -gt 0; do
-	case "$1" in
-	-h|--help)
-
-		return 0 2>/dev/null; exit 0
-		;;
-	-p*|--port*)
-		if ! grep -q "=" <<< $1; then shift; fi
-		port=`option_value $1`
-		shift
-		;;
-	-l*|--language*)
-		if ! grep -q "=" <<< $1; then shift; fi
-		language=`option_value $1`
-		shift
-		;;
-	-ro|--raw-output)
-		raw_output="true"
-		shift
-		;;
-	*|--)
-		break
-		;;
-	esac
-done
 
 # Functions
 printf_n(){ printf "$1\n" "${@:2}"; }
@@ -39,24 +14,44 @@ main() {
 	sudo apt install jq -y &>/dev/null
 	# Texts
 	if [ "$language" = "RU" ]; then
+		#local t_re="\n${C_R}Вы не зарегистрировали ноду!${RES}
 
+#${C_LGn}Для регистрации необходимо${RES}:
+#1) Перейти на сайт: https://incentivecash.minima.global/
+#2) Авторизоваться
+#3) Скопировать ID ноды
+#4) Выполнить команду ниже и вставить ID ноды
+#. <(wget -qO- https://raw.githubusercontent.com/CHINGIZ8/minima/main/multi_tool.sh) -rg\n"
 		local t_re="\n${C_R}Либо не зарегистрирована нода, либо некорректно работает RPC, который не починить!${RES}\n"
 		local t_nv="\nВерсия ноды:            ${C_LGn}%s${RES}"
 		local t_lb="Текущий блок:         ${C_LGn}%s${RES}\n"
 		
-		local t_ni="\nID ноды:                ${C_LGn}%s${RES}"
-		local t_rc="Реферальный код:        ${C_LGn}%s${RES}"
-		local t_lp="Последний сигнал:       ${C_LGn}%s${RES} (UTC)"
 		
-		local t_r="\n\n\tНаграды\n"
-		local t_af="Нода после форка:       ${C_LGn}%d${RES}"
-		local t_bf="Нода до форка:          ${C_LGn}%d${RES}"
-		local t_cc="Вклад в сообщество:     ${C_LGn}%d${RES}"
-		local t_ir="Приглашение рефералов:  ${C_LGn}%d${RES}"
 		
-
+	# Send Pull request with new texts to add a language - https://github.com/CHINGIZ8/minima/main/node_info.sh
+	#elif [ "$language" = ".." ]; then
 	else
+		#local t_re="\n${C_R}You haven't registered the node!${RES}
 
+#${C_LGn}To register you need to${RES}:
+#1) Go to the site: https://incentivecash.minima.global/
+#2) Log in
+#3) Copy the node ID
+#4) Execute the command below and enter the node ID
+#. <(wget -qO- https://raw.githubusercontent.com/CHINGIZ8/minima/main/multi_tool.sh) -rg\n"
+		local t_re="\n${C_R}Either the node is not registered, or the RPC does not work correctly, which cannot be fixed!${RES}\n"
+		local t_nv="\nNode version:            ${C_LGn}%s${RES}"
+		local t_lb="Latest block height:     ${C_LGn}%s${RES}\n"
+		
+		local t_ni="Node ID:                 ${C_LGn}%s${RES}"
+		local t_rc="Refferal code:           ${C_LGn}%s${RES}"
+		local t_lp="Last ping:               ${C_LGn}%s${RES} (UTC)"
+		
+		local t_r="\n\n\tRewards\n"
+		local t_af="Node after fork:         ${C_LGn}%d${RES}"
+		local t_bf="Node before fork:        ${C_LGn}%d${RES}"
+		local t_cc="Community contribution:  ${C_LGn}%d${RES}"
+		local t_ir="Inviting referrals:      ${C_LGn}%d${RES}"
 	fi
 
 	# Actions
