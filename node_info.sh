338091c9f380e222c3d6a8ac3cae84b5ff9ce69c
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2023-01-27 10:22:53 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -68,7 +68,7 @@ main() {
 #. <(wget -qO- https://raw.githubusercontent.com/CHINGIZ8/minima/main/multi_tool.sh) -rg\n"
 		local t_re="\n${C_R}Либо не зарегистрирована нода, либо некорректно работает RPC, который не починить!${RES}\n"
 		local t_nv="\nВерсия ноды:            ${C_LGn}%s${RES}"
-		local t_lb="Последний блок:         ${C_LGn}%s${RES}\n"
+		local t_lb="Последний блок2:         ${C_LGn}%s${RES}\n"
 		
 		local t_ni="\nID ноды:                ${C_LGn}%s${RES}"
 		local t_rc="Реферальный код:        ${C_LGn}%s${RES}"
