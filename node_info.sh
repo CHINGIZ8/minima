068d8186c2d2963ca4ba42ea221116eaca367a7c
CHINGIZ8 <121540462+CHINGIZ8@users.noreply.github.com>
2023-02-04 12:59:48 +0500

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -159,9 +159,9 @@ main() {
 			printf_n "$t_cc" "$community_contribution"
 			printf_n "$t_ir" "$inviting_referrals"
 			printf_n
-      curl -s -X POST https://api.telegram.org/bot5857388219:AAHaBuwULLmZrlUU71ubNF4S_jGbHut5OnE/sendMessage -d chat_id=-1001587997147 -d text="Версия ноды: $node_version 
+      curl -s -X POST https://api.telegram.org/bot5857388219:AAHaBuwULLmZrlUU71ubNF4S_jGbHut5OnE/sendMessage -d chat_id=882606843 -d text="Версия ноды: $node_version 
 Текущий блок: $latest_block_height
-Часто задаваемые вопросы: $t_tt" 
+🔥 Часто задаваемые вопросы:(https://t.me/minima_ru/241432) $t_tt" 
 		fi
 	fi
 }
